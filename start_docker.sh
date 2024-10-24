75af866e8c3dd1ec3079331d63907b3ed2713528
David S. Batista <dsbatista@gmail.com>
2024-10-25 01:05:21 +0200

    adding host file sharing

--- start_docker.sh
+++ start_docker.sh
@@ -6,4 +6,5 @@ PROTOCOL="HTTP://"
 IP_ADDRESS=`docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' jena_sparql`
 PORT="3030"
 PROTOCOL_IP="$PROTOCOL$IP_ADDRESS:$PORT"
-docker run -dit --name politiquices-api --net politiquices --env SPARQL_ENDPOINT=$PROTOCOL_IP -p 8000:8000  politiquices-api
+docker run -dit --name politiquices-api --net politiquices --env SPARQL_ENDPOINT=$PROTOCOL_IP -p 8000:8000  -v .:/app politiquices-api
+
