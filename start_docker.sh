02deb5d4709b3bb7d31ea77fd9ee9b611d7c0719
David S. Batista <dsbatista@gmail.com>
2026-08-12 10:30:02 +0200

    update para 127.0.0.1:8000:8000

--- start_docker.sh
+++ start_docker.sh
@@ -6,5 +6,5 @@ PROTOCOL="HTTP://"
 IP_ADDRESS=`docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' jena_sparql`
 PORT="3030"
 PROTOCOL_IP="$PROTOCOL$IP_ADDRESS:$PORT"
-docker run -dit --name politiquices-api --net politiquices --env SPARQL_ENDPOINT=$PROTOCOL_IP -p 8000:8000  -v .:/app politiquices-api
+docker run -dit --name politiquices-api --net politiquices --env SPARQL_ENDPOINT=$PROTOCOL_IP -p 127.0.0.1:8000:8000  -v .:/app politiquices-api
 
